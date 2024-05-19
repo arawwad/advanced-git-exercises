6c6d5db7f39e83e4d0d3289fc0d07e26ededa1aa
Abdallah Awwad <ar.awwad6@gmail.com>
2024-05-19 14:15:46 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -13,8 +14,9 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
+	directory := flag.String("directory", "", "File Read Directory")
+	flag.Parse()
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -26,11 +28,11 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleRequest(conn)
+		go handleRequest(conn, *directory)
 	}
 }
 
-func handleRequest(conn net.Conn) {
+func handleRequest(conn net.Conn, directory string) {
 	buffer := make([]byte, 1024)
 	conn.Read(buffer)
 	request := string(buffer)
@@ -56,6 +58,23 @@ func handleRequest(conn net.Conn) {
 		userAgentRegex := regexp.MustCompile(`User-Agent: (\S+)`)
 		userAgent := userAgentRegex.FindStringSubmatch(request)[1]
 		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", utf8.RuneCountInString(userAgent), userAgent)
+	}
+
+	if strings.HasPrefix(target, "/files/") {
+		fileNameRegex := regexp.MustCompile(`/files/(.*)`)
+		fileName := fileNameRegex.FindStringSubmatch(target)
+
+		if fileName != nil {
+			file, _ := os.ReadFile(fmt.Sprintf("%s/%s", directory, fileName[1]))
+			str := string(file)
+
+			if file != nil {
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", utf8.RuneCountInString(str), str)
+			}
+
+		}
+
+		fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
 	}
 
 	fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
